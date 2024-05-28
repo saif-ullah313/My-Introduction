b9b58732c35aac3e2de8271f0b13421d6bc7bee7
saif-ullah313 <saifullahkhanjadoon1998@gmail.com>
2024-05-28 15:46:34 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Profile from './Profile'
+import './App.css'
+import './index.css'
+
+
+function App() {
+
+  return (
+    <>
+      <Profile/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Profile.jsx
@@ -0,0 +1,12 @@
+function Profile() {
+    return (
+        <div className="parent">
+            <h1>My Introduction</h1>
+            <p>My name is Saifullah</p>
+            <p>My Father name is Muhammad Riaz Khan</p>
+            <p>I have master in CS</p>
+            <p>I am from Havelian Abbottabad kpk Pakistan</p>
+        </div>
+    )
+}
+export default Profile;
